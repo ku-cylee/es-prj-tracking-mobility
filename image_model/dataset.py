f5612a97e2721a7c5e124f3335761a85a54f3b69
Chanyoung Lee <btstar_cy@korea.ac.kr>
2022-05-24 02:40:09 +0900

    Implement Trainer and Dataset

---
+++ image_model/dataset.py
@@ -0,0 +1,57 @@
+import os
+import torch
+import numpy as np
+
+from torch.utils.data import Dataset
+
+class ImageDataset(Dataset):
+
+    def __init__(self, data_dir, is_train):
+        data_files = os.listdir(data_dir)
+        
+        labels = list(set(self.get_label_from_filename(filename) for filename in data_files))
+        self.labels = Labels(labels)
+
+        if is_train:
+            data_files = [df for idx, df in enumerate(data_files) if idx % 5 != 0]
+        else:
+            data_files = [df for idx, df in enumerate(data_files) if idx % 5 == 0]
+
+        input_tensors = []
+        output_tensors = []
+
+        for filename in data_files:
+            label_name = self.get_label_from_filename(filename)
+            input_tensors.append(torch.FloatTensor(np.load(os.path.join(data_dir, filename))))
+            output_tensors.append(self.labels.get_index(label_name))
+
+        self.input = torch.stack(input_tensors)
+        self.output = torch.LongTensor(output_tensors)
+
+
+    def __len__(self):
+        return len(self.output)
+
+
+    def __getitem__(self, index):
+        return self.input[index], self.output[index]
+
+
+    def get_label_from_filename(self, filename):
+        return filename.split('_')[0]
+
+
+class Labels:
+
+    def __init__(self, labels_list):
+        self.idx_to_name = labels_list
+        self.name_to_idx = {label: idx for idx, label in enumerate(labels_list)}
+
+    def get_name(self, index):
+        return self.idx_to_name[index]
+
+    def get_index(self, name):
+        return self.name_to_idx[name]
+
+    def size(self):
+        return len(self.idx_to_name)
---
+++ image_model/trainer.py
@@ -0,0 +1,131 @@
+import argparse
+import time
+import torch
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
+
+from torch.utils.data import DataLoader
+
+from model import IdentityResNet
+from dataset import ImageDataset
+
+class ImageModelTrainer:
+
+    def __init__(self, args):
+        print('Trainer Initializing...')
+
+        self.train_batch_size = 8
+        self.test_batch_size = 4
+        self.epoch_size = 2
+        self.data_dir = './data/converted'
+
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+        self.trainloader, self.testloader = self.get_dataset()
+
+        self.net = IdentityResNet(nblk_stage1=2, nblk_stage2=2, nblk_stage3=2, nblk_stage4=2)
+        self.model = self.net.to(self.device)
+
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = optim.SGD(self.model.parameters(), lr=1e-2, momentum=.9)
+
+
+    def get_dataset(self):
+        transform = transforms.Compose([
+            # transforms.ToTensor(),
+            transforms.Normalize((.5, .5, .5), (.5, .5, .5)),
+        ])
+
+        trainset = ImageDataset(self.data_dir, is_train=True)
+        trainloader = DataLoader(trainset, batch_size=self.train_batch_size, shuffle=True)
+
+        testset = ImageDataset(self.data_dir, is_train=False)
+        testloader = DataLoader(testset, batch_size=self.test_batch_size, shuffle=True)
+
+        self.labels = trainset.labels
+
+        return trainloader, testloader
+
+
+    def train(self):
+        print('Trainer Training...')
+        for epoch_idx in range(self.epoch_size):
+            print(f'  Running Epoch {epoch_idx + 1}/{self.epoch_size}...')
+
+            report_loss = 0
+            report_started_at = time.time()
+            REPORT_PERIOD = 100
+            for batch_idx, batch_data in enumerate(self.trainloader):
+                local_loss = self.batchwise_train(batch_data)
+                report_loss += local_loss
+
+                if (batch_idx + 1) % 100:
+                    continue
+
+                current_time = time.time()
+                avg_loss = report_loss / REPORT_PERIOD
+                elapsed_time = current_time - report_started_at
+                print(f'    Batch {epoch_idx + 1}.{batch_idx + 1}: Loss {avg_loss:.6f}, Elapsed Time {elapsed_time:.6f}(s)')
+                report_loss = 0
+                report_started_at = current_time
+
+        print('Trainer Training END')
+
+
+    def batchwise_train(self, batch):
+        inputs, labels = batch[0].to(self.device), batch[1].to(self.device)
+        self.model.zero_grad()
+        outputs = self.model(inputs)
+        loss = self.criterion(outputs, labels)
+
+        loss.backward()
+        self.optimizer.step()
+
+        return loss.item()
+
+
+    def evaluate(self):
+        print('Trainer Evaluating...')
+        correct_counts = [0 for _ in range(self.labels.size())]
+        total_counts = [0 for _ in range(self.labels.size())]
+
+        with torch.no_grad():
+            for batch in self.testloader:
+                inputs, labels = batch[0].to(self.device), batch[1].to(self.device)
+                outputs = self.net(inputs)
+                predicts = (torch.max(outputs, 1)[1] == labels).squeeze()
+                for label, predict in zip(labels, predicts):
+                    correct_counts[label] += predict.item()
+                    total_counts[label] += 1
+
+        print('  Evaluation Report')
+        print(f'    Overall Accurracy: {100 * sum(correct_counts) / sum(total_counts)}%')
+
+        for label_idx in range(self.labels.size()):
+            correct = correct_counts[label_idx]
+            total = total_counts[label_idx]
+            percentage = 100 * correct / total
+            print(f'    {self.labels.get_name(label_idx)}: {percentage:.4f}% ({correct}/{total})')
+        
+        print('Training Evaluating END')
+
+
+    def export(self):
+        pass
+
+
+    def run(self):
+        self.train()
+        self.evaluate()
+        self.export()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    args = parser.parse_args()
+    trainer = ImageModelTrainer(args)
+    trainer.run()
+else:
+    raise ImportError('This module is not for import')
