257367a0e5cbdd36cc9ec907c775b3a3e6a46a83
Chanyoung Lee <btstar_cy@korea.ac.kr>
2022-06-17 03:44:49 +0900

    Remove unnecessary code

--- src/car_controller.py
+++ src/car_controller.py
@@ -1,21 +1,6 @@
-import os
 import argparse
 
-from internal.lib import mkdir_nonexist
-from internal.inference import get_trained_model, get_image_tensor, infer, Centroid
 from internal.car_ctrl import CarController
-
-def main(src_dir, dst_dir, model_path, train_size, split_count):
-    model = get_trained_model(model_path)
-    sample_filenames = os.listdir(src_dir)
-    for idx, filename in enumerate(sample_filenames):
-        image_size = train_size * split_count
-        _, sample = get_image_tensor(src_dir, filename, image_size)
-        print(f'Inferring {filename}: ({idx + 1}/{len(sample_filenames)})')
-        output = infer(model, sample, train_size, split_count)
-        centroid = Centroid(output, train_size, split_count)
-        centroid.save_plot(src_dir, dst_dir, filename)
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
